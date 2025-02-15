06954fbf48b2538304f9655c3a972107433c8760
profesorinfo <62035356+ihavenomouth@users.noreply.github.com>
2025-02-15 22:03:11 +0100

    limpieza

--- rrhh/routes/loginRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import loginController from '../controllers/LoginController.js';
-
-router.route('/')
-  .post( loginController.postLogin )
-;
-
-export default router;
